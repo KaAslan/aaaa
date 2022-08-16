ec6877451b72ff3165ce09db2e38783dc219cbb7
KaAslan <lacivertnil2@gmail.com>
2022-08-16 22:08:14 +0300

    v4

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -2,6 +2,7 @@ public class Day1Git {
     public static void main(String[] args) {
         System.out.println("hello Git 1. Deneme");
         System.out.println("hello Git 2. Deneme");
+        System.out.println("hello Git 3. Deneme");
 
 
 
